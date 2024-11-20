e938cf0cf706320a328b9e85b44d0e475a08baed
Wilkuuu <arek.szumacher@gmail.com>
2024-11-20 16:33:35 +0100

    add

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from time import sleep
+
 import cv2
 import torch
 from ultralytics import YOLO
 # import torch
 import utils
+import os
 
-
+image_dir = 'images'
 
 model = YOLO("yolo11l-pose.pt")
+
 
 # Adjust brightness (beta) and contrast (alpha)
 def adjust_brightness_contrast(frame, alpha=1.0, beta=50):
@@ -16,6 +20,7 @@ def adjust_brightness_contrast(frame, alpha=1.0, beta=50):
     beta: Brightness control (0-100)
     """
     return cv2.convertScaleAbs(frame, alpha=alpha, beta=beta)
+
 
 KEYPOINT_LABELS = [
     "Nose", "Left Eye", "Right Eye", "Left Ear", "Right Ear",
@@ -28,17 +33,19 @@ KEYPOINT_LABELS = [
 def main():
     # droidcam_url = "http://192.168.8.120:4747/video"
     # cap = cv2.VideoCapture(droidcam_url)
-    cap = cv2.VideoCapture(0)  # Open the webcam (0 is usually the default webcam)
-    alpha = .7  # Contrast
-    beta = .2
+    # cap = cv2.VideoCapture(0)  # Open the webcam (0 is usually the default webcam)
+    # alpha = .7  # Contrast
+    # beta = .2
+    for f in os.listdir(image_dir):
+        image_path = os.path.join(image_dir, f)
+        frame = cv2.imread(image_path)
+        # while cap.isOpened():
+        #     ret, frame = cap.read()
+        #     if not ret:
+        #         print("Failed to grab frame")
+        #         break
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("Failed to grab frame")
-            break
-
-        frame = adjust_brightness_contrast(frame, alpha=alpha, beta=beta)
+        # frame = adjust_brightness_contrast(frame, alpha=alpha, beta=beta)
         # Run pose detection
         results = model(frame)  # Inference with YOLOv8 pose model
         results_tensor = torch.tensor(results[0].keypoints.xy)
@@ -49,7 +56,7 @@ def main():
 
         # Display the result
         for label, coord in merged_keypoints.items():
-            print(f"{label}: {coord}")
+          print(f"{label}: {coord}")
 
         annotated_frame = results[0].plot()  # YOLOv8 provides a plotting function to visualize results
 
@@ -57,6 +64,7 @@ def main():
         cv2.imshow('Pose Detection', annotated_frame)
 
         # Break the loop on 'q' key press
+        sleep(1)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
